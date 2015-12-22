f4759f5aa0b4f3f6f99bdf130eddc1824512cd27
Nick Pizzo <nick.pizzo@gmail.com>
2015-12-22 12:37:58 -1000

    fist commit

---
+++ scripts/StorePicker.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+//
+// var StorePicker = React.createClass({
+//   mixins: [History],
+//   goToStore: function (event) {
+//     event.preventDefault();
+//     //get the data from input
+//     var storeId = this.refs.storeId.value
+//     //re-route to app
+//     this.history.pushState(null, '/store/' + storeId);
+//   },
+//
+//   render: function () {
+//     return (
+//       <form className="store-selector" onSubmit={this.goToStore}>
+//         <h2>Please Enter A Store</h2>
+//         <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
+//         <input type="Submit" />
+//       </form>
+//     )
+//   }
+// })
+//
+// export default StorePicker
